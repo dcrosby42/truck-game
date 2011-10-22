ed9dec6f615b049d107a37a082b1d1784ccf194a
David Crosby <dcrosby42@gmail.com>
2011-10-22 01:57:58 -0400

    BG needed to be loaded with a hard edge

--- lib/background_factory.rb
+++ lib/background_factory.rb
@@ -4,6 +4,6 @@ class BackgroundFactory
   constructor :media_loader
 
   def build(opts)
-    Background.new(:image => @media_loader.load_image(opts[:image_name]))
+    Background.new(:image => @media_loader.load_image(opts[:image_name], true))
   end
 end
